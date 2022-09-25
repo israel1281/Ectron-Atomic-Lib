5e4a622cb72999844eea0869598559b1bf48db75
Israel__david <kidoski1@gmail.com>
2022-09-25 15:22:14 +0000

    Update 12 files

---
+++ .eslintrc.js
@@ -0,0 +1,34 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    "next/core-web-vitals",
+    "plugin:react/recommended",
+    "google",
+    "prettier",
+  ],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 13,
+    sourceType: "module",
+  },
+  plugins: ["react", "@typescript-eslint"],
+  overrides: [
+    // Only uses Testing Library lint rules in test files
+    {
+      files: ["**/__tests__/**/*.[jt]s?(x)", "**/?(*.)+(spec|test).[jt]s?(x)"],
+      // extends: ["plugin:testing-library/react"],
+    },
+  ],
+  rules: {},
+  settings: {
+    react: {
+      version: "detect",
+    },
+  },
+};
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,6 @@
 import type { NextPage } from "next";
 import Head from "next/head";
-import Image from "next/image";
+import React from "react";
 import styles from "../styles/Home.module.css";
 import Input from "../atoms/input";
 import Button from "../atoms/button";
